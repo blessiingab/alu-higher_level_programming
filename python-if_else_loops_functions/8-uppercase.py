e62409ec260847eff86d54cb36d240810cf86c78
blessiingab <b.ingabire1@alustudent.com>
2025-02-18 09:26:13 -0800

    uppercase

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,10 +1,4 @@
 #!/usr/bin/python3
-def uppercase(str):
-    result = ""
-    for char in str:
-        if 'a' <= char <= 'z':  # Check if lowercase
-            result += chr(ord(char) - 32)  # Convert to uppercase
-        else:
-            result += char  # Keep non-lowercase characters unchanged
-    print(result)
-
+def print_last_digit(number):
+    print(abs(number) % 10, end="")
+    return (abs(number) % 10)
