b909068d63ae6c7f47fc83696af0908b45537e65
blessiingab <b.ingabire1@alustudent.com>
2025-02-18 09:30:18 -0800

    uppercase

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
 def print_last_digit(number):
-    print(abs(number) % 10, end="")
-    return (abs(number) % 10)
+    # First print function, using string formatting
+    print("Last digit: {}".format(abs(number) % 10), end="")  # Prints formatted string
+    return abs(number) % 10  # Returns last digit
+
