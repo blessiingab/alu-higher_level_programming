a6039a57583969588e9f2a9627894c1a14c81bd0
blessiingab <b.ingabire1@alustudent.com>
2025-02-20 03:25:59 -0800

    matrix

--- python-more_data_structures/101-square_matrix_map.py
+++ python-more_data_structures/101-square_matrix_map.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
-def complex_delete(a_dictionary, value):
-    for key, val in list(a_dictionary.items()):
-        if val == value:
-            del a_dictionary[key]
-    return a_dictionary
+def square_matrix_map(matrix=[]):
+    return list(map(lambda row: list(map(lambda x: x**2, row)), matrix))
