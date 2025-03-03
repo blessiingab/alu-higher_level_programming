98c58cf45ea2d99ff5aec8fee5508c7a4322b4fa
blessiingab <b.ingabire1@alustudent.com>
2025-03-03 04:59:45 -0800

    linked list

--- python-classes/100-singly_linked_list.py
+++ python-classes/100-singly_linked_list.py
@@ -7,6 +7,7 @@ This module contains two classes:
 - SinglyLinkedList: Implements a singly linked list with sorted insertion.
 """
 
+
 class Node:
     """Defines a node of a singly linked list.
     
@@ -14,22 +15,23 @@ class Node:
         data (int): The data stored in the node.
         next_node (Node or None): Reference to the next node in the list.
     """
-    
+
     def __init__(self, data, next_node=None):
         """Initialize a node with data and optional next_node.
         
         Args:
             data (int): The data value of the node.
-            next_node (Node, optional): The next node in the linked list. Defaults to None.
+            next_node (Node, optional): The next node in the linked list.
+                Defaults to None.
         """
         self.data = data
         self.next_node = next_node
-    
+
     @property
     def data(self):
         """Getter for data attribute."""
         return self.__data
-    
+
     @data.setter
     def data(self, value):
         """Setter for data attribute with validation.
@@ -43,12 +45,12 @@ class Node:
         if not isinstance(value, int):
             raise TypeError("data must be an integer")
         self.__data = value
-    
+
     @property
     def next_node(self):
         """Getter for next_node attribute."""
         return self.__next_node
-    
+
     @next_node.setter
     def next_node(self, value):
         """Setter for next_node attribute with validation.
@@ -70,11 +72,11 @@ class SinglyLinkedList:
     Attributes:
         __head (Node or None): The head (first node) of the linked list.
     """
-    
+
     def __init__(self):
         """Initialize an empty singly linked list."""
         self.__head = None
-    
+
     def sorted_insert(self, value):
         """Inserts a new Node into the correct sorted position (increasing order).
         
@@ -91,7 +93,7 @@ class SinglyLinkedList:
                 current = current.next_node
             new_node.next_node = current.next_node
             current.next_node = new_node
-    
+
     def __str__(self):
         """Returns a string representation of the linked list.
         
